a4b5dd15b9b2446f22d91ada36d8a230ae8aefab
quang_techasia <quangdv@techasia.biz>
2021-07-21 11:15:35 +0700

    up

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
         $(this).children("img").toggleClass("active");
         $(this).children("span").toggleClass("active");
     });
-    
+
     $(".show-box-detail-service").click(function(){
         $(this).parents(".item-box-price").toggleClass("active");
     });
     $(function(){
         $(".show-video-list-servece").click(function () {
-           $(this).children(".animation-text-video").text(function(i, text){
-               return text === "動画で簡単説明" ? "閉じる" : "動画で簡単説明";
-           });
-           $(this).children(".animation-text-video").css("transition","all .4s")
+            $(this).children(".animation-text-video").text(function(i, text){
+                return text === "動画で簡単説明" ? "閉じる" : "動画で簡単説明";
+            });
+            $(this).children(".animation-text-video").css("transition","all .4s")
         });
         $(".show-box-detail-service").click(function () {
             $(this).children(".animation-box-detail").text(function(i, text){
@@ -26,8 +26,8 @@ $(document).ready(function(){
             $(this).children(".animation-text-map").text(function(i, text){
                 return text === "MAPを見る" ? "MAPを閉じる" : "MAPを見る";
             })
-         });
-     })
+        });
+    })
 
     $('#slide-gym').owlCarousel({
         center: true,
@@ -127,15 +127,26 @@ $(document).ready(function(){
         }, 'slow');
         e.preventDefault();
     });
-    // 
+    //
     function swipMobile(){
         let widthOffset = $(document).width();
         console.log(widthOffset);
-        // if(widthOffset > 1080){
-        //     $(".box-background").click(() => $(".carousel").carousel("next"));
-        // }
+        if(widthOffset <= 1080){
+            $(".carousel").swipe({
+
+                swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+                    if (direction == 'left') $(this).carousel('next');
+                    if (direction == 'right') $(this).carousel('prev');
+
+                },
+                allowPageScroll:"vertical"
+
+            });
+        } else{
+            $(".box-background").click(() => $(this).carousel("next"));
+        }
     }
-    $(".box-background").click(() => $(".carousel").carousel("next"));
     // 
     let d = new Date();
     document.getElementById("get-year").innerHTML = d.getFullYear();
@@ -154,8 +165,8 @@ $(document).ready(function(){
     // 
     let prevScrollpos = window.pageYOffset;
     $(window).scroll(function() {
-    let heightHeader = $("header#header").outerHeight();
-    let currentScrollPos = window.pageYOffset;
+        let heightHeader = $("header#header").outerHeight();
+        let currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
             document.getElementById("header").style.top = "0";
         } else {
